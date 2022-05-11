eacef1c54cfd49e8e8766edcea7b3597c96dce71
berkaykopuz <berkaykopuz@hotmail.com>
2022-05-11 13:45:51 +0300

    Web Site Source

--- Source/control.php
+++ Source/control.php
@@ -1,13 +1,33 @@
 <?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$email = $_POST['email'];
+$password = $_POST['password']; 
 
-echo $username;
+if($email == "g211210045@sakarya.edu.tr"){
+    if($password == "g211210045"){
+        sleep(2);
+        echo '<h1>Hoşgeldiniz G211210045! <a href="index.html"> Ana sayfaya buraya tıklayarak dönebilirsiniz </a></h1>';
+        
+    }
+    else {
+        
+        
+        header("Location: login.html");
+        
+       
 
+    }
+} 
+else {
+        
+        header("Location: login.html");
+}
 
 
 
 
 
 ?>
+
+
+
